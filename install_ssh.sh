d4d8613f17d703e423cb5333d39e6861bc04bfac
aksenov-fx <161599647+aksenov-fx@users.noreply.github.com>
2024-10-23 04:03:48 +0200

    update ssh script

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y openssh-server
+#Install ssh
+    sudo apt update
+    sudo apt install -y openssh-server
 
-sudo systemctl enable ssh --now
+    sudo systemctl enable ssh --now
 
-sudo ufw allow ssh
+    sudo ufw allow ssh
 
-curl https://github.com/aksenov-fx.keys >> ~/.ssh/authorized_keys
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
+# Add ssh key
+    curl https://github.com/aksenov-fx.keys >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh
+    chmod 600 ~/.ssh/authorized_keys
+
+# Download ssh config
+    sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+    sudo wget -P /etc/ssh/ https://raw.githubusercontent.com/aksenov-fx/bash/main/sshd_config
 
 # Disable password authentication
-sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sudo wget -P /etc/ssh/ https://raw.githubusercontent.com/aksenov-fx/bash/main/sshd_config
+    #sudo sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+    #sudo sed -i 's/^#\?PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 
-#sudo sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-#sudo sed -i 's/^#\?PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-
+# Restart ssh
 sudo systemctl restart ssh
