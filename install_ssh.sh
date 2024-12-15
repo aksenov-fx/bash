74e88840605f8e934a0bed7fd63bbb16fb3ae25d
aksenov-fx <161599647+aksenov-fx@users.noreply.github.com>
2024-12-15 15:32:36 +0100

    update ssh script

--- install_ssh.sh
+++ install_ssh.sh
@@ -4,24 +4,30 @@ set -euxo pipefail
 
 #Install ssh
     sudo apt update
-    sudo apt install -y openssh-server
+    sudo apt install -y openssh-server curl
 
     sudo systemctl enable ssh --now
 
-    sudo ufw allow ssh
+    if [ -x "$(command -v ufw)" ]; then
+        sudo ufw allow ssh
+    fi
 
 # Add ssh key
+    mkdir -p ~/.ssh
     curl https://github.com/aksenov-fx.keys >> ~/.ssh/authorized_keys
     chmod 700 ~/.ssh
     chmod 600 ~/.ssh/authorized_keys
-
-# Download ssh config
-    sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-    sudo wget -P /etc/ssh/ https://raw.githubusercontent.com/aksenov-fx/bash/main/sshd_config
 
 # Disable password authentication
     #sudo sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
     #sudo sed -i 's/^#\?PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 
+# Download ssh config
+    timestamp=$(date +%Y%m%d_%H%M%S)
+    sudo mv /etc/ssh/sshd_config "/etc/ssh/sshd_config_${timestamp}.bak"
+    sudo wget -P /etc/ssh/ https://raw.githubusercontent.com/aksenov-fx/bash/main/sshd_config
+
 # Restart ssh
     sudo systemctl restart ssh
+
+echo SSH was configured successfully
